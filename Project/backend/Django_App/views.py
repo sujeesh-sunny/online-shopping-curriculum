469f05f2395dd853db39a5356afea6b64cebd993
sujeesh sunny <sujeeshsunnyssk1@gmail.com>
2024-10-31 16:55:32 +0530

    Added React and Django project files

---
+++ Project/backend/Django_App/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all().values()
+    return JsonResponse(list(products), safe=False)
+
